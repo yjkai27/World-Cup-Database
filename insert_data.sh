1cf55a0235b44695609eda6b28f4b217ef2be98d
david yunseong jung <145014381+yjkai27@users.noreply.github.com>
2023-09-26 22:41:48 +0900

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,23 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[$YEAR != "year"]]
+  then
+    WINNER_TEAM=$($PSQL "SELECT team_id FROM teams WHERE team=$WINNER")
+    if [[-z $WINNER_TEAM]]
+    then
+      INSERT_WINNER=$($PSQL "INSERT INTO teams(team) VALUES($WINNER)")
+    fi
+    OPPONENT_TEAM=$($PSQL "SELECT team_id FROM teams WHERE team=$OPPONENT")
+    if [[-z $OPPONENT_TEAM]]
+    then
+      INSERT_OPPONENT=$($PSQL "INSERT INTO teams(team) VALUES($OPPONENT)")
+    fi
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE team=$WINNER")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE team=$OPPONENT")
+    INSERT_GAME=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  fi
+  
+done
